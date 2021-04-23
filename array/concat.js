f507324718408304161576dd8e5cb617d4498e68
felipeghidini <lipeghidini18@gmail.com>
2021-04-22 21:34:34 -0300

    Imperativo VS Declarativo e Concat

---
+++ array/concat.js
@@ -0,0 +1,7 @@
+const filhas = ['Valaska', 'Cibalena']
+const filhos = ['Valter', 'Uescley']
+const todos = filhas.concat(filhos, 'Fulano')
+console.log(todos, filhos, filhas)
+
+console.log(['a', 'b'].concat([1, 2], [3, 4], 5, [[6, 7]]
+    ))
