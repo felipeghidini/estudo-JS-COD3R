bfa3a7529ed7d9106ea3815858ded0d6454dad03
felipeghidini <lipeghidini18@gmail.com>
2021-07-06 20:42:12 -0300

    Estudos Promises

---
+++ promises/promises_2.js
@@ -0,0 +1,12 @@
+const primeiroElemento = array => array[0];
+const primeiraLetra = string => string[0];
+const letraMinuscula = letra => letra.toLowerCase();
+
+let p = new Promise(resolve => {
+    resolve(['Ana', 'Bia', 'Carlos', 'Daniel'])
+})
+p.then(primeiroElemento)
+ .then(primeiraLetra)
+ .then(letraMinuscula)
+ .then(console.log);
+
